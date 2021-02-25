e50995ef47df71620de28ab2c6f3888c4976c791
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-02-25 18:56:58 -0300

    Day 3

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Profile } from "../components/Profile";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import Head from 'next/head';
 
@@ -23,7 +24,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
