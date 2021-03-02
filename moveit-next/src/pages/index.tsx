a04a5615b91b0a84fe0b118d1d6f51d4dab3cfe7
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2021-03-02 19:27:19 -0300

    Added fancier Tab Navigation with Session Storage

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,16 +1,33 @@
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
 import { Profile } from "../components/Profile";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ChallengeBox } from "../components/ChallengeBox";
 
-import Head from 'next/head';
-
 import styles from '../styles/pages/Home.module.css'
 import { CountdownProvider } from "../contexts/CountdownContext";
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
+
+  console.log(props);
+  
+
   return (
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
     <div className={styles.container}>
       <Head>
         <title>Início | move.it</title>
@@ -31,5 +48,18 @@ export default function Home() {
         </section>
       </CountdownProvider>
     </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
